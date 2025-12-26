966a019bcece975cfbf27a3f9f043599d7698dc9
Pavel Roskin <plroskin@gmail.com>
2025-12-25 22:33:36 -0800

    Add copyright to files, credit Rust developers for expand.rs

--- src/locate.rs
+++ src/locate.rs
@@ -1,3 +1,11 @@
+// Copyright 2025 Pavel Roskin
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use std::env;
 use std::ffi::OsStr;
 use std::path::Path;
