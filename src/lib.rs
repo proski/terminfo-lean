d9e6a6c0b32414f619454610721581d7546ce25b
Pavel Roskin <plroskin@gmail.com>
2025-12-26 18:57:28 -0800

    Add crate level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,32 @@
 // except according to those terms.
 
 //! Terminfo parsing library with simple API and minimal dependencies
+//!
+//! This crate provides facilities to
+//!
+//! * find the terminfo database for the given terminal
+//! * parse the terminfo database and
+//! * expand capabilities with parameters.
+//!
+//! Features:
+//!
+//! * full support for extended capabilities
+//! * simple API
+//! * extensive unit test coverage
+//!
+//! Why another terminfo library?
+//!
+//! * MIT + Apache 2.0 license (no obscenities or obscure licenses)
+//! * minimal dependencies (`thiserror` only)
+//! * truly lean - no termcap, no Windows console, no unrelated stuff
+//! * UTF-8 is only used for capability names
+//! * 8-bit clean - string capabilities are byte slices
+//! * minimal memory allocations
+//!
+//! Credits
+//!
+//! The capability expansion code is based on the `term` crate with
+//! significant changes.
 
 pub mod expand;
 pub mod locate;
